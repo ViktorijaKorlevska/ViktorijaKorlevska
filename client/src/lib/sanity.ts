843cf4a9addc410ba99067a906900bb55cd08fa8
ViktorijaKorlevska <dodovaviktorija@gmail.com>
2026-02-03 17:16:24 +0100

    sanity api version

--- client/src/lib/sanity.ts
+++ client/src/lib/sanity.ts
@@ -3,7 +3,7 @@ import { createClient } from '@sanity/client';
 export const sanityClient = createClient({
   projectId: import.meta.env.SANITY_PROJECT_ID,
   dataset: import.meta.env.SANITY_DATASET,
-  apiVersion: import.meta.env.SANITY_API_VERSION,
+  apiVersion: import.meta.env.SANITY_API_VERSION || '2024-01-01',
   useCdn: true
 });
 
